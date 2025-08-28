c3180aa1e28a27505dfbdb1311c185db69cb151b
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2025-08-28 14:14:48 +0600

    some changes on traversal & insertion file

--- Linked List/Traversal and Insertion/main.cpp
+++ Linked List/Traversal and Insertion/main.cpp
@@ -28,90 +28,90 @@ int main() {
 // ............................................................................................
 // insertion on linkedlist at start
 
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// class Node {
-// public:
-//     int data;
-//     Node *next;
+class Node {
+public:
+    int data;
+    Node *next;
 
-//     Node(int value) {
-//         data = value;
-//         next = NULL;
-//     }
-// };
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
 
-// int main() {
-//     Node *Head;
-//     Head = NULL;
+int main() {
+    Node *Head;
+    Head = NULL;
 
-//     int arr[] = {2, 4, 6, 8, 10};
+    int arr[] = {2, 4, 6, 8, 10};
     
-//     for(int i=0; i<5; i++) {
-//         if(Head == NULL) {  // linked list doesn't exist
-//             Head = new Node(arr[i]);
-//         }else {  // linked list exists
-//             Node *temp;
-//             temp = new Node(arr[i]);
-//             temp -> next = Head;
-//             Head = temp;
-//         }
-//     }
+    for(int i=0; i<5; i++) {
+        if(Head == NULL) {  // linked list doesn't exist
+            Head = new Node(arr[i]);
+        }else {  // linked list exists
+            Node *temp;
+            temp = new Node(arr[i]);
+            temp -> next = Head;
+            Head = temp;
+        }
+    }
 
-//     // print the value
+    // print the value
 
-//     Node *temp = Head;
+    Node *temp = Head;
 
-//     while(temp!=NULL) {
-//         cout << temp -> data << " ";
-//         temp = temp -> next;
-//     }
-// }
+    while(temp!=NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+}
 
 // output:
 // 10 8 6 4 2
 // ............................................................................................
 // insertion at ending
 
-// #include<bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
 
-// class Node {
-// public:
-//     int data;
-//     Node *next;
+class Node {
+public:
+    int data;
+    Node *next;
 
-//     Node(int value) {
-//         data = value;
-//         next = NULL;
-//     }
-// };
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
 
-// int main() {
-//     Node *Head, *Tail;
-//     Tail = Head = NULL;
+int main() {
+    Node *Head, *Tail;
+    Tail = Head = NULL;
 
-//     int arr[] = {2, 4, 6, 8, 10};
+    int arr[] = {2, 4, 6, 8, 10};
 
-//     for(int i=0; i<5; i++) {
-//         if(Head == NULL) {  // linked list is empty
-//             Head = new Node(arr[i]);
-//             Tail = Head;
-//         }else {  // linked list exists
-//             Tail -> next = new Node(arr[i]);
-//             Tail = Tail -> next;
-//         }
-//     }
+    for(int i=0; i<5; i++) {
+        if(Head == NULL) {  // linked list is empty
+            Head = new Node(arr[i]);
+            Tail = Head;
+        }else {  // linked list exists
+            Tail -> next = new Node(arr[i]);
+            Tail = Tail -> next;
+        }
+    }
 
-//     Node *temp;
-//     temp = Head;
+    Node *temp;
+    temp = Head;
 
-//     while(temp!=NULL) {
-//         cout << temp -> data << " ";
-//         temp = temp -> next;
-//     }
-// }
+    while(temp!=NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+}
 
 // output:
 // 2 4 6 8 10 
