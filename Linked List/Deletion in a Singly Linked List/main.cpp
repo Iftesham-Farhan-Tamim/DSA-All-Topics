458878fc6fdc73450d316a1eb58504d92904e3c0
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2025-09-04 20:53:31 +0600

    some updates

--- Linked List/Deletion in a Singly Linked List/main.cpp
+++ Linked List/Deletion in a Singly Linked List/main.cpp
@@ -1,6 +1,297 @@
+// delete first node on linked list
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    cout << "hello world";
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node *CreateLinkedList(int arr[], int index, int size) {
+    if(index == size) {
+        return NULL;
+    }
+
+    Node *temp = new Node(arr[index]);
+    temp -> next = CreateLinkedList(arr, index+1, size);
+    return temp;
 }
+
+int main() {
+    Node *Head;
+    Head = NULL;
+
+    int arr[] = {2, 4, 6, 8, 10};
+
+    Head = CreateLinkedList(arr, 0, 5);
+
+    if(Head!=NULL) {
+        Node *temp = Head;
+        Head = Head -> next;
+        delete temp;
+    }
+
+    while(Head) {
+        cout << Head -> data << " ";
+        Head = Head -> next;
+    }
+}
+
+// output:
+// 4 6 8 10
+// ............................................................................................
+// delete last node on linked list
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node *CreateLinkedList(int arr[], int index, int size) {
+    if (index == size) {
+        return NULL;
+    }
+
+    Node *temp = new Node(arr[index]);
+    temp->next = CreateLinkedList(arr, index + 1, size);
+    return temp;
+}
+
+int main() {
+    Node *Head;
+    Head = NULL;
+
+    int arr[] = {2, 4, 6, 8, 10};
+
+    Head = CreateLinkedList(arr, 0, 5);
+
+    if (Head != NULL) {
+        if (Head -> next == NULL) {  // only one node is present
+            Node *temp = Head;
+            delete temp;
+            Head = NULL;
+        } else {  // more than one node is present
+            Node *curr = Head;
+            Node *prev = NULL;
+
+            // curr -> next is not NULL
+
+            while (curr->next != NULL) {
+                prev = curr;
+                curr = curr->next;
+            }
+
+            prev->next = NULL;
+            delete curr;
+        }
+    }
+
+    while (Head) {
+        cout << Head->data << " ";
+        Head = Head->next;
+    }
+}
+
+// output:
+// 2 4 6 8
+// ............................................................................................
+// delete particular node on linked list
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+int main() {
+    Node *Head, *Tail;
+    Head = Tail = NULL;
+
+    int arr[] = {2, 4, 6, 8, 10};
+    int x = 3;
+
+    for(int i=0; i<5; i++) {
+        if(Head == NULL) {
+            Head = new Node(arr[i]);
+            Tail = Head;
+        } else {
+            Tail -> next = new Node(arr[i]);
+            Tail = Tail -> next;
+        }
+    }
+
+    if(x==1) {
+        Node *temp = Head;
+        Head = Head -> next;
+        delete temp;
+    } else {
+        x--;
+        Node *curr = Head;
+        Node *prev = NULL;
+
+        while(x--) {
+            prev = curr;
+            curr = curr -> next;
+        }
+
+        prev -> next = curr -> next;
+        delete curr;
+    }
+
+    // print final list
+    Node *temp;
+    temp = Head;
+
+    while(temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+// output:
+// 2 4 8 10
+// ............................................................................................
+// delete particular node on linked list using recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node *deleteNode(Node *curr, int x) {
+    if(x==1) {
+        Node *temp;
+        temp = curr -> next;
+        delete curr;
+        return temp;
+    }
+
+    curr -> next = deleteNode(curr -> next, x-1);
+    return curr;
+}
+
+int main() {
+    Node *Head, *Tail;
+    Tail = Head = NULL;
+
+    int arr[] = {2, 4, 6, 8, 10};
+    int x = 3;
+
+    for(int i=0; i<5; i++) {
+        if(Head == NULL) {
+            Head = new Node(arr[i]);
+            Tail = Head;
+        } else {
+            Tail -> next = new Node(arr[i]);
+            Tail = Tail -> next;
+        }
+    }
+
+    Head = deleteNode(Head, x);
+
+    Node *temp;
+    temp = Head;
+
+    while(temp!=NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+}
+
+// output:
+// 2 4 8 10
+// ............................................................................................
+// delete without head pionter
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+// delete given node without head
+void deleteNode(Node *curr) {
+    Node *temp = curr->next;
+    curr->data = temp->data;
+    curr->next = temp->next;
+    delete temp;
+}
+
+int main() {
+    Node *Head, *Tail;
+    Head = Tail = NULL;
+
+    int arr[] = {2, 4, 6, 8, 10};
+    int n = 5;
+    int x = 3;
+
+    // create linked list
+    for(int i=0; i<n; i++) {
+        if(Head == NULL) {
+            Head = new Node(arr[i]);
+            Tail = Head;
+        } else {
+            Tail->next = new Node(arr[i]);
+            Tail = Tail->next;
+        }
+    }
+
+    // Suppose we want to delete 3rd node (value 6) without head
+    Node *temp = Head;
+    for(int i=1; i<x; i++) {  
+        temp = temp->next;
+    }  
+
+    deleteNode(temp);
+
+    // print updated list
+    temp = Head;
+    while(temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+// output:
+// 2 4 8 10
--- Linked List/Traversal and Insertion/main.cpp
+++ Linked List/Traversal and Insertion/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 class Node {
-    public:
+public:
     int data;
     Node *next;
 
@@ -16,7 +16,7 @@ class Node {
 
 int main() {
     Node *Head;
-    Head = new Node(4);
+    Head = new Node(4);  // new is a dynamic memory allocation operator.
 
     cout << Head -> data << endl;
     cout << Head -> next << endl;
@@ -49,13 +49,13 @@ int main() {
     int arr[] = {2, 4, 6, 8, 10};
     
     for(int i=0; i<5; i++) {
-        if(Head == NULL) {  // linked list doesn't exist
+        if(Head == NULL) {  
             Head = new Node(arr[i]);
-        }else {  // linked list exists
+        } else {  
             Node *temp;
             temp = new Node(arr[i]);
-            temp -> next = Head;
-            Head = temp;
+            temp -> next = Head;  // link new node to the existing list
+            Head = temp;  // update Head to point to the new node
         }
     }
 
@@ -98,7 +98,7 @@ int main() {
         if(Head == NULL) {  // linked list is empty
             Head = new Node(arr[i]);
             Tail = Head;
-        }else {  // linked list exists
+        } else {  // linked list exists
             Tail -> next = new Node(arr[i]);
             Tail = Tail -> next;
         }
@@ -115,3 +115,162 @@ int main() {
 
 // output:
 // 2 4 6 8 10 
+// ............................................................................................
+// creating linked list using recursion adding note at end
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node *CreateLinkedList(int arr[], int index, int size) {
+    // base case
+    if(index==size) {
+        return NULL;
+    }
+
+    Node *temp;
+    temp = new Node(arr[index]);
+    temp -> next = CreateLinkedList(arr, index+1, size);
+
+    return temp;  // head of the current sublist
+}
+
+int main() {
+    Node *Head, *Tail;
+    Tail = Head = NULL;
+
+    int arr[] = {2, 4, 6, 8, 10};
+    Head = CreateLinkedList(arr, 0, 5);
+
+    Node *temp;
+    temp = Head;
+
+    while(temp!=NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+}
+
+// output:
+// 2 4 6 8 10 
+// ............................................................................................
+// inserting node at begining using recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node *CreateLinkedList(int arr[], int index, int size, Node *prev) {
+    // base case
+    if(index == size) {
+        return prev;
+    }
+
+    Node *temp;
+    temp = new Node(arr[index]);
+    temp -> next = prev;
+    return CreateLinkedList(arr, index+1, size, temp);
+}
+
+int main() {
+    Node *Head;
+    Head = NULL;
+
+    int arr[] = {2, 4, 6, 8, 10};
+
+    Head = CreateLinkedList(arr, 0, 5, NULL);
+
+    Node *temp;
+    temp = Head;
+
+    while(temp!=NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+}
+
+// output:
+// 10 8 6 4 2
+// ............................................................................................
+// inserting node at particular position
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node *CreateLinkedList(int arr[], int index, int size) {
+    // base case
+    if(index == size) {
+        return NULL;
+    }
+
+    Node *temp;
+    temp = new Node(arr[index]);
+    temp -> next = CreateLinkedList(arr, index+1, size);
+
+    return temp;    
+}
+
+int main() {
+    Node *Head;
+    Head = NULL;
+
+    int arr[] = {2, 4, 6, 8, 10};
+
+    Head = CreateLinkedList(arr, 0, 5);
+
+    int x = 3;
+    int value = 30;
+
+    Node *temp;
+    temp = Head;
+    x--;
+
+    while(x--) {
+        temp = temp -> next;
+    }
+
+    Node *temp2 = new Node(value);
+    temp2 -> next = temp -> next;
+    temp -> next = temp2;
+
+    temp = Head;
+
+    while(temp!=NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+}
+
+// output:
+// 2 4 6 30 8 10
